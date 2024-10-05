fb81e6992c61b345aa5dfae9ef9ac9544a99ec05
Flo1ther <shevchuk.nirosla@gmail.com>
2024-10-05 13:22:58 +0300

    Добавьте файлы проекта.

---
+++ LINQList/Program.cs
@@ -0,0 +1,48 @@
+﻿using LINQList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+        Console.OutputEncoding = Encoding.UTF8;
+
+        List<Student> students = new List<Student>
+        {
+            new Student("Вероніка", 18, 11),
+            new Student("Андрій", 15, 11),
+            new Student("Артем", 15, 10),
+            new Student("Олег", 15, 10),
+            new Student("Сергій", 19, 11),
+            new Student("Тимур", 15, 10),
+            new Student("Мирослав", 17, 10),
+            new Student("Юрій", 16, 12),
+            new Student("Іван", 16, 10),
+            new Student("Андрій", 15, 10),
+        };
+
+        Console.WriteLine("Початковий список студентів:");
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
+
+        Console.WriteLine("\nСортування за віком:");
+        var sortedByAge = students.OrderBy(s => s.Age);
+        foreach (var student in sortedByAge)
+        {
+            Console.WriteLine(student);
+        }
+
+        Console.WriteLine("\nСортування за середнім балом (спадання):");
+        var sortedByGradeDesc = students.OrderByDescending(s => s.Grade);
+        foreach (var student in sortedByGradeDesc)
+        {
+            Console.WriteLine(student);
+        }
+
+        Console.WriteLine("\nСортування за ім'ям:");
+        var sortedByName = students.OrderBy(s => s.Name);
+        foreach (var student in sortedByName)
+        {
+            Console.WriteLine(student);
+        }
---
+++ LINQList/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQList
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string name, int age, double grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"Ім'я: {Name}, Вік: {Age}, Середній бал: {Grade}";
+        }
+    }
+}
